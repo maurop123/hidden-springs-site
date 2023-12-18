871b80351532eae95ce99fe42680f1ed670ccbc4
Mauro P <maurop123@gmail.com>
2023-12-18 08:11:10 -0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <div className="grid grid-cols-12">
+        <div className="col-span-6 bg-pink-600 min-h-screen">
+
+        </div>
+
+        <div className="col-span-6 bg-green-600 min-h-screen">
+
+        </div>
+      </div>
+    </main>
+  )
+}
